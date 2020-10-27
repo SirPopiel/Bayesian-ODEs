2e2f1aec45393189607508a2717dfb672d20c176
broccoloff <federicofatone18@gmail.com>
2020-10-27 16:04:48 +0100

    Nuovi commenti
    Aggiunti commenti sulla parte bayesiana

--- BDP_2/LV_noisy/example.py
+++ BDP_2/LV_noisy/example.py
@@ -159,7 +159,7 @@ if __name__ == "__main__":
     # Until here no Bayesian framework is considered. It is just to provide the model with
     #  a starting point 
 
-    initial_weight = parameters_pre
+    initial_weight = parameters_pre  # We initialize the weights with the parameters found in preconditioning
     print(initial_weight.shape, "here")
 
 
@@ -167,46 +167,50 @@ if __name__ == "__main__":
         def __init__(self):
             super(ODEModel, self).__init__()
             self.Weights = tf.Variable(tf.random.normal([num_param, 1], dtype=tf.float32)*0.01, dtype=tf.float32)
-
+            # Initializer, initializes the weight to normal random variables with sd = 0.01
 
         def call(self, inputs, **kwargs):
             t, y = inputs
             h = y
-            h1 = h[:,0:1]
-            h2 = h[:,1:2]
+            h1 = h[:, 0:1]
+            h2 = h[:, 1:2]
 
             p1 = self.Weights[0]
             p2 = self.Weights[1]
             p3 = self.Weights[2]  
             p4 = self.Weights[3] 
 
-            h_out1 = p1 * h1 + sigma_y * p2 * h2*h1 
-            h_out2 = p3 * h2 + sigma_x * p4 * h2*h1
+            h_out1 = p1 * h1 + sigma_y * p2 * h2*h1  # ?? As before, why sigma_y?
+            h_out2 = p3 * h2 + sigma_x * p4 * h2*h1  # ?? As before, why sigma_x?
             h_out = tf.concat([h_out1, h_out2], 1)
             return h_out
 
 
     model = ODEModel()
-    neural_ode = NeuralODE(model, t = t_in)
+    neural_ode = NeuralODE(model, t=t_in)  # We assign to NeuralODE the just created model and the time grid
+    # between t0 and t1
 
     @tf.function
     def compute_gradients_and_update(batch_y0, batch_yN): 
         """Takes start positions (x0, y0) and final positions (xN, yN)"""
-        pred_y = neural_ode.forward(batch_y0)
+        pred_y = neural_ode.forward(batch_y0)  # This finds the predicted yNs
         with tf.GradientTape() as g:
             g.watch(pred_y)
-            loss = tf.reduce_sum(input_tensor=(pred_y - batch_yN)**2)
+            loss = tf.reduce_sum(input_tensor=(pred_y - batch_yN)**2)  # This creates the loss function
             
-        dLoss = g.gradient(loss, pred_y)
-        h_start, dfdh0, dWeights = neural_ode.backward(pred_y, dLoss)
+        dLoss = g.gradient(loss, pred_y)  # This computes the gradient of the loss function
+        h_start, dfdh0, dWeights = neural_ode.backward(pred_y, dLoss)  # This applies the gradient descent to find
+        # the updates for the weights
 
         return loss, dWeights
 
     # Compile EAGER graph to static (this will be much faster)
-    #compute_gradients_and_update = tfe.defun(compute_gradients_and_update)
+    # compute_gradients_and_update = tfe.defun(compute_gradients_and_update)
 
+    #########################################################################################################
+    # We now start the Bayesian framework
 
-    # function to compute the kinetic energy
+    # function to compute the kinetic energy, for Hamiltonian Monte Carlo (to be seen with Guglielmi)
     def kinetic_energy(V, loggamma_v, loglambda_v):
         q = (np.sum(-V**2) - loggamma_v**2 - loglambda_v**2)/2.0
         return q
@@ -219,6 +223,7 @@ if __name__ == "__main__":
     def compute_gradient_hyper(loss, weights, loggamma, loglambda, batch_size, para_num):
         grad_loggamma = np.exp(loggamma) * (loss/2.0 + 1.0) - (batch_size/2.0 + 1.0)
         grad_loglambda = np.exp(loglambda) * (np.sum(np.abs(weights)) + 1.0) - (para_num + 1.0)
+        # This somehow computes the gradient of the hyper parameters in order to update them from step to step
 
         return grad_loggamma, grad_loglambda
 
@@ -228,7 +233,9 @@ if __name__ == "__main__":
         return H
 
     def leap_frog(v_in, w_in, loggamma_in, loglambda_in, loggamma_v_in, loglambda_v_in):
-        # assign weights from the previous step to the model
+        # In pratica, non viene utilizzato il gradient descent, ma il leapfrog, algoritmo per ottimizzazione
+        #  unconstrained implementato in questa porzione di codice... Ci fidiamo...
+
         model.trainable_weights[0].assign(w_in)
         v_new = v_in
         loggamma_v_new = loggamma_v_in
@@ -269,56 +276,64 @@ if __name__ == "__main__":
         return v_new, w_new, loggamma_new, loglambda_new, loggamma_v_new, loglambda_v_new
 
 
-
     neural_ode_test = NeuralODE(model, t=t_grid[0:data_size:20])
-    parameters = np.zeros((niters, para_num)) # book keeping the parameters
-    loggammalist = np.zeros((niters, 1)) # book keeping the loggamma
-    loglambdalist = np.zeros((niters, 1)) # book keeping the loggamma
-    loglikelihood = np.zeros((niters, 1)) # book keeping the loggamma
-    L = 10 # leap frog step number
-    epsilon = 0.001 # leap frog step size
+    parameters = np.zeros((niters, para_num))  # book keeping the parameters
+    loggammalist = np.zeros((niters, 1))  # book keeping the loggamma
+    loglambdalist = np.zeros((niters, 1))  # book keeping the loggamma
+    loglikelihood = np.zeros((niters, 1))  # book keeping the loggamma
+    L = 10  # leap frog step number
+    epsilon = 0.001  # leap frog step size
     epsilon_max = 0.0002    # max 0.001
     epsilon_min = 0.0002    # max 0.001
     
 
     def compute_epsilon(step):
+        # This will compute the epsilon to use in the leapfrog which is different for every step. It decreases
+        # with the steps increasing in number
         coefficient = np.log(epsilon_max/epsilon_min)
-        return epsilon_max * np.exp( - step * coefficient / niters)
+        return epsilon_max * np.exp(- step * coefficient / niters)
 
 
     # initial weight
-    w_temp = initial_weight
+    w_temp = initial_weight  # The one we found from the preconditioner
     print("initial_w", w_temp)
     loggamma_temp = 4. + np.random.normal()
     loglambda_temp = np.random.normal()
 
-    model.trainable_weights[0].assign(w_temp)
-    loss_original, _ = compute_gradients_and_update(batch_y0, batch_yN) # compute the initial Hamiltonian
+    model.trainable_weights[0].assign(w_temp)  # We assign to the weights of the model, the ones we found through
+    # the pre conditioner. Remember that the initial weights set by the initializer were random distributed according
+    # to a Gaussian with 0 mean and 0.01 sd.
+    loss_original, _ = compute_gradients_and_update(batch_y0, batch_yN)  # Compute the initial Hamiltonian
 
+    loggamma_temp = np.log(batch_size / loss_original)  # We define an initial guess for loggamma ?? Why defined as such?
 
-    loggamma_temp = np.log(batch_size / loss_original)
     print("This is initial guess", loggamma_temp, "with loss", loss_original)
     if loggamma_temp > 6.:
         loggamma_temp = 6.
         epsilon_max = 0.0002
         epsilon_min = 0.0002
 
-
     # training steps
     for step in range(niters):
 
-        epsilon = compute_epsilon(step)
+        epsilon = compute_epsilon(step)  # Compute the adaptive epsilon for the steps
 
         print(step)
-        v_initial = np.random.randn(para_num, 1) # initialize the velocity
+
+        v_initial = np.random.randn(para_num, 1)  # initialize the velocity
         loggamma_v_initial = np.random.normal()
         loglambda_v_initial = np.random.normal()
 
-        loss_initial, _ = compute_gradients_and_update(batch_y0, batch_yN) # compute the initial Hamiltonian
+        loss_initial, _ = compute_gradients_and_update(batch_y0, batch_yN)  # compute the initial Hamiltonian
+        # This line uses the weights of the preconditioner (see line 303) to compute the loss function with those
         loss_initial = compute_Hamiltonian(loss_initial, w_temp, loggamma_temp, loglambda_temp, batch_size, para_num)
+        # Then it computes the Hamiltonian
 
         v_new, w_new, loggamma_new, loglambda_new, loggamma_v_new, loglambda_v_new = \
                                 leap_frog(v_initial, w_temp, loggamma_temp, loglambda_temp, loggamma_v_initial, loglambda_v_initial)
+
+        # Then the leapfrog is applied in order to update the parameters and the hyper parameters and to further
+        # optimize the weights estimation (L steps of leapfrog at a time)
 
         # compute the final Hamiltonian
         loss_finial, _ = compute_gradients_and_update(batch_y0, batch_yN)
@@ -331,7 +346,7 @@ if __name__ == "__main__":
         p = min(1, p_temp)
         p_decision = np.random.uniform()
         if p > p_decision:
-            parameters[step:step+1, :] = np.transpose(w_new)
+            parameters[step:step+1, :] = np.transpose(w_new)  # Parameters are updated
             w_temp = w_new
             loggammalist[step, 0] = loggamma_new
             loglambdalist[step, 0] = loglambda_new
@@ -339,7 +354,7 @@ if __name__ == "__main__":
             loggamma_temp = loggamma_new
             loglambda_temp = loglambda_new
         else:
-            parameters[step:step+1, :] = np.transpose(w_temp)
+            parameters[step:step+1, :] = np.transpose(w_temp)  # New parameters are not updated
             model.trainable_weights[0].assign(w_temp)
             loggammalist[step, 0] = loggamma_temp
             loglambdalist[step, 0] = loglambda_temp
@@ -348,7 +363,6 @@ if __name__ == "__main__":
         print('probability', p)
         print(p > p_decision)
 
-
-    np.save('parameters', parameters)
-    np.save('loggammalist', loggammalist)
-    np.save('loglikelihood', loglikelihood)
+    np.save('parameters', parameters)  # The Monte Carlo chain of the parameters
+    np.save('loggammalist', loggammalist)  # The Monte Carlo chain of loggamma
+    np.save('loglikelihood', loglikelihood)  # The Monte Carlo chain of losses
